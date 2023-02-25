e74b1af6ea71fd007c5c2115d046308df291cf37
Margarita <podezzhikh95@bk.ru>
2023-02-25 23:59:05 +0200

    Adding class AI and User

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class Dot:
     def __init__(self, x, y):
         self.x = x
@@ -150,3 +153,30 @@ class Player:
                 return repeat
             except BoardException as e:
                 print(e)
+
+
+class AI(Player):
+    def ask(self):
+        d = Dot(randint(0, 5), randint(0, 5))
+        print(f"Ход компьютера: {d.x + 1} {d.y + 1}")
+        return d
+
+
+class User(Player):
+    def ask(self):
+        while True:
+            cords = input("Введите координаты клетки через пробел: ").split()
+
+            if len(cords) != 2:
+                print("Введите 2 координаты!")
+                continue
+
+            x, y = cords
+
+            if not (x.isdigit()) or not (y.isdigit()):
+                print("Введите числа!")
+                continue
+
+            x, y = int(x), int(y)
+
+            return Dot(x - 1, y - 1)
