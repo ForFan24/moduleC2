dc4cb921a1bb5b1f80cbcd633b744c19c08f5d46
Margarita <podezzhikh95@bk.ru>
2023-02-25 23:54:00 +0200

    Adding class Player

--- game.py
+++ game.py
@@ -133,3 +133,20 @@ class Board:
     def begin(self):
         self.busy = []
 
+
+class Player:
+    def __init__(self, board, enemy):
+        self.board = board
+        self.enemy = enemy
+
+    def ask(self):
+        raise NotImplementedError()
+
+    def move(self):
+        while True:
+            try:
+                target = self.ask()
+                repeat = self.enemy.shot(target)
+                return repeat
+            except BoardException as e:
+                print(e)
